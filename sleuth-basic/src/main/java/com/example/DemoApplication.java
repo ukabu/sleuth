673dd453aeb8b42e5c8a237b90f032de8ce05c07
Ukabu <emmanuel@pirsch.org>
2017-05-02 14:41:09 -0400

    Ajout du bean RestTemplate

--- sleuth-basic/src/main/java/com/example/DemoApplication.java
+++ sleuth-basic/src/main/java/com/example/DemoApplication.java
@@ -25,6 +25,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
 
 @RestController
