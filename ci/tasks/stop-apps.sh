0694a47eeec07ab1cca6544c495827202889e3ff
Ukabu <emmanuel@pirsch.org>
2017-05-08 11:21:54 -0400

    Ajout des tâches de déploiement et d'arrêt des appications

---
+++ ci/tasks/stop-apps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+set +x
+cf login -a {{pcf-api}} -u {{pcf-username}} -p {{pcf-password}} -o {{pcf-org}} -s {{pcf-space}}
+set -x
+
+cf stop sleuth-message-client
+cf stop sleuth-message-service
+cf stop sleuth-zipkin-query-service
+
+cf logout
+
+exit 0
